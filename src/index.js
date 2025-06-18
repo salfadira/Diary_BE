914bb23da1edbd1804f703eba6c6eb6c84fcdd77
salfadira <salfadiraputrii@gmail.com>
2025-06-18 20:54:12 +0700

    update lagi

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const pool = require("./db");
+const db = require("./db");
 
 
 const app = express();
