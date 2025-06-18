c28e446cffc692f89a684cb33ad51723529a29c0
salfadira <salfadiraputrii@gmail.com>
2025-06-18 20:25:42 +0700

    Perbaikan ke 2

--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const pool = require("./db");
 
-require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 // Middleware
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors());
 app.use(bodyParser.json());
 
 // Routes
